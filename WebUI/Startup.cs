5c6a6efb183d7b5443f3a8dbcc7b26831bfa2c37
raj ratre <ratre.raj@gmail.com>
2022-02-17 22:10:34 +0530

    changes in design

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -65,7 +65,7 @@ namespace WebUI
                 );
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=SignIn}/{id?}");
             });
         }
     }
